39a318157a218c790e40c9ab6727ec36d6400c64
Kaio Sérgio Sales Nunes <137112682+Kaiosergio21@users.noreply.github.com>
2025-05-14 11:47:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const mysql = require('mysql2');
 const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8000;
-
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -23,26 +21,30 @@ db.connect((err) => {
   console.log('Conexão com o banco de dados MySQL estabelecida com sucesso!');
 });
 
+// Serve arquivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota principal exibindo produtos
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public','views' ,'Home.html'));
+  db.query('SELECT * FROM produto', (err, results) => {
+    if (err) throw err;
+    res.sendFile(path.join(__dirname, 'public', 'views', 'Home.html')); // Envia o arquivo HTML
+  });
 });
 
+app.use('/imagens_div', express.static(path.join(__dirname, 'public/imagens_div')));
+
+
 app.get('/produtos', (req, res) => {
-  const query = 'SELECT * FROM produto';
-  
-  db.query(query, (err, results) => {
+  db.query('SELECT * FROM produto', (err, results) => {
     if (err) {
       console.error('Erro ao buscar produtos:', err.message);
-      return res.status(500).json({ error: 'Erro ao buscar produtos.' });
+      return res.status(500).json({ error: 'Erro ao buscar produtos' });
     }
-
-    res.json(results);
+    res.json(results); // Envia os produtos como resposta JSON
   });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-  });
-
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
